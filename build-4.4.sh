d8916e6e6c6178bd0b10619d0b49448d193ec77a
attack11 <fernandobouchet@gmail.com>
2021-03-04 20:52:48 -0300

    scripts: Reorder setup and append 4.4 version

--- build-4.4.sh
+++ build-4.4.sh
@@ -6,24 +6,24 @@ BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 # Release type
 if [[ $BRANCH == "extended" ]]; then
     if [[ "$@" =~ "oldcam" ]]; then
-	curl -s -X POST https://api.telegram.org/bot${BOT_API_TOKEN}/sendMessage -d text="<i><b>Extended Hmp ${STABLE_RELEASE_VERSION} build's started on CI...</b></i>" -d chat_id=${KERNEL_CHAT_ID_PUBLIC} -d parse_mode=HTML
-	export VERSION="Kernel-Hmp-Oldcam-${STABLE_RELEASE_VERSION}"
+	curl -s -X POST https://api.telegram.org/bot${BOT_API_TOKEN}/sendMessage -d text="<i><b>Extended Hmp 4.4 ${STABLE_RELEASE_VERSION} build's started on CI...</b></i>" -d chat_id=${KERNEL_CHAT_ID_PUBLIC} -d parse_mode=HTML
+	export VERSION="Kernel-4.4-Hmp-Oldcam-${STABLE_RELEASE_VERSION}"
 	elif [[ "$@" =~ "newcam" ]]; then
-	export VERSION="Kernel-Hmp-Newcam-${STABLE_RELEASE_VERSION}"
+	export VERSION="Kernel-4.4-Hmp-Newcam-${STABLE_RELEASE_VERSION}"
 	fi
 elif [[ $BRANCH == "extended-eas" ]]; then
     if [[ "$@" =~ "oldcam" ]]; then
-	curl -s -X POST https://api.telegram.org/bot${BOT_API_TOKEN}/sendMessage -d text="<i><b>Extended Eas ${STABLE_RELEASE_VERSION} build's started on CI...</b></i>" -d chat_id=${KERNEL_CHAT_ID_PUBLIC} -d parse_mode=HTML
-	export VERSION="Kernel-Eas-Oldcam-${STABLE_RELEASE_VERSION}"
+	curl -s -X POST https://api.telegram.org/bot${BOT_API_TOKEN}/sendMessage -d text="<i><b>Extended Eas 4.4 ${STABLE_RELEASE_VERSION} build's started on CI...</b></i>" -d chat_id=${KERNEL_CHAT_ID_PUBLIC} -d parse_mode=HTML
+	export VERSION="Kernel-4.4-Eas-Oldcam-${STABLE_RELEASE_VERSION}"
 	elif [[ "$@" =~ "newcam" ]]; then
-	export VERSION="Kernel-Eas-Newcam-${STABLE_RELEASE_VERSION}"
+	export VERSION="Kernel-4.4-Eas-Newcam-${STABLE_RELEASE_VERSION}"
 	fi
 else
     if [[ "$@" =~ "oldcam" ]]; then
 	curl -s -X POST https://api.telegram.org/bot${BOT_API_TOKEN}/sendMessage -d text="<i><b>Extended Beta ${BRANCH} build's started on CI...</b></i>" -d chat_id=${KERNEL_CHAT_ID_PRIVATE} -d parse_mode=HTML
-	export VERSION="Kernel-Beta-Eas-Oldcam-${BRANCH}-${SEMAPHORE_WORKFLOW_NUMBER}"
+	export VERSION="Kernel-4.4-Beta-Eas-Oldcam-${BRANCH}-${SEMAPHORE_WORKFLOW_NUMBER}"
 	elif [[ "$@" =~ "newcam" ]]; then
-	export VERSION="Kernel-Beta-Eas-Newcam-${BRANCH}-${SEMAPHORE_WORKFLOW_NUMBER}"
+	export VERSION="Kernel-4.4-Beta-Eas-Newcam-${BRANCH}-${SEMAPHORE_WORKFLOW_NUMBER}"
 	fi
 fi
 
@@ -36,6 +36,14 @@ export ZIPNAME="Extended-${VERSION}.zip"
 # Set COMPILER
 export ARCH=arm64 && export SUBARCH=arm64
 export KBUILD_JOBS="$((`grep -c '^processor' /proc/cpuinfo` * 2))"
+if [[ "$@" =~ "clang" ]]; then
+	export PATH="$(pwd)/clang/bin:$PATH"
+	make -j${KBUILD_JOBS} O=out ARCH=arm64 CC="clang" CROSS_COMPILE="aarch64-linux-gnu-" CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
+elif [[ "$@" =~ "gcc" ]]; then
+    export CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-elf-"
+	export CROSS_COMPILE_ARM32="$(pwd)/gcc32/bin/arm-eabi-"
+	make -j${KBUILD_JOBS} O=out ARCH=arm64
+fi
 
 # Compilation
 if [[ "$@" =~ "oldcam" ]]; then
@@ -46,14 +54,6 @@ fi
 START=$(date +"%s")
 make O=out clean && make O=out mrproper
 make O=out ARCH=arm64 $DEFCONFIG
-if [[ "$@" =~ "clang" ]]; then
-	export PATH="$(pwd)/clang/bin:$PATH"
-	make -j${KBUILD_JOBS} O=out ARCH=arm64 CC="clang" CROSS_COMPILE="aarch64-linux-gnu-" CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
-elif [[ "$@" =~ "gcc" ]]; then
-    export CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-elf-"
-	export CROSS_COMPILE_ARM32="$(pwd)/gcc32/bin/arm-eabi-"
-	make -j${KBUILD_JOBS} O=out ARCH=arm64
-fi
 END=$(date +"%s")
 DIFF=$((END - START))
 
