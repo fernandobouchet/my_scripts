117646f075513d455ff27292bf09ca4be1582854
attack11 <fernandobouchet@gmail.com>
2021-05-01 21:06:11 -0300

    Add updated glibc for gcc

--- setup.sh
+++ setup.sh
@@ -8,4 +8,16 @@ if [[ "$@" =~ "clang" ]]; then
 elif [[ "$@" =~ "gcc" ]]; then
 	git clone https://github.com/arter97/arm64-gcc.git -b master --depth=1 gcc
 	git clone https://github.com/arter97/arm32-gcc.git -b master --depth=1 gcc32
+	cd ..
+	mkdir ~/glibc_install
+	cd ~/glibc_install
+	wget http://ftp.gnu.org/gnu/glibc/glibc-2.33.tar.gz
+	tar zxvf glibc-2.33.tar.gz
+	cd glibc-2.33
+	mkdir build
+	cd build
+	../configure –prefix=/opt/glibc-2.33
+	make -j4
+	sudo make install -y
+	export LD_LIBRARY_PATH=/opt/glibc-2.33/lib
 fi
