774d3758a078c4dd9abee98dc378fd19503d72ca
attack11 <fernandobouchet@gmail.com>
2021-06-22 21:27:36 -0300

    Revert "scripts: update"
    This reverts commit 919665342d0c9bbe6bcafc8069b59f95e74029f6.

--- setup.sh
+++ setup.sh
@@ -8,4 +8,9 @@ if [[ "$@" =~ "clang" ]]; then
 elif [[ "$@" =~ "gcc" ]]; then
 	git clone https://github.com/arter97/arm64-gcc.git -b master --depth=3 gcc
 	git clone https://github.com/arter97/arm32-gcc.git -b master --depth=3 gcc32
+	cd gcc
+	git checkout cd9eb72bace3b4d682d5251a9eb4829bdd0ec2ca
+	cd ../gcc32
+	git checkout b788b457799d68553f51a00a5dd4a1d0ea6b0558
+	cd ..
 fi
