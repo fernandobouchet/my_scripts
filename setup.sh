a11d4ab8e6b95b452b4921b63968a849b1cdc9f1
Fernando Bouchet <fernandobouchet@gmail.com>
2021-11-21 18:28:33 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
 
-git clone https://github.com/attack11/AnyKernel3 -b Extended-Kernel --depth=1 anykernel
+git clone https://github.com/fernandobouchet/AnyKernel3 -b Extended-Kernel --depth=1 anykernel
 
 if [[ "$@" =~ "clang" ]]; then
 	git clone https://github.com/kdrag0n/proton-clang.git --depth=1 clang
